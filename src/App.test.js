22eb4513e06fc37ba75ef0bd095c1ea08aa85a05
Guy <ezraguy1@gmail.com>
2022-09-16 14:58:38 +0300

    adding add and subtract functionality testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import Counter from './components/Counter';
 import '@testing-library/jest-dom/extend-expect';
@@ -13,4 +13,32 @@ test('initial value of the counter is 0', () => {
   const { getByTestId } = render(<Counter />);
   const CounterValue = getByTestId('counter-value');
   expect(CounterValue.textContent).toBe('0');
+});
+
+test('add button gets rendered with the correct symbol', () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId('add-btn');
+  expect(addBtn.textContent).toBe('+');
+});
+
+test('subtract button gets rendered with the correct symbol', () => {
+  const { getByTestId } = render(<Counter />);
+  const subBtn = getByTestId('sub-btn');
+  expect(subBtn.textContent).toBe('-');
+});
+
+test('counter value goes up by 1 when clicking on the add button', () => {
+  const { getByTestId } = render(<Counter />);
+  const addBtn = getByTestId('add-btn');
+  const CounterValue = getByTestId('counter-value');
+  fireEvent.click(addBtn);
+  expect(CounterValue.textContent).toBe('1');
+});
+
+test('counter value goes down by 1 when clicking on the subtract button', () => {
+  const { getByTestId } = render(<Counter />);
+  const subBtn = getByTestId('sub-btn');
+  const CounterValue = getByTestId('counter-value');
+  fireEvent.click(subBtn);
+  expect(CounterValue.textContent).toBe('-1');
 });
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,22 @@ import React, { useState } from 'react';
 
 const Counter = () => {
   const [counterAmount, setCounterAmount] = useState(0);
+  const handleClick = (action) => {
+    setCounterAmount(action === 'add' ? counterAmount + 1 : counterAmount - 1);
+  };
   return (
     <div className="counter">
       <h3 className="title">My Counter</h3>
       <div data-testid="counter-value" className="counter-amount">
         {counterAmount}
+      </div>
+      <div className="buttons">
+        <div data-testid="add-btn" className="add-btn" onClick={() => handleClick('add')}>
+          +
+        </div>
+        <div data-testid="sub-btn" className="sub-btn" onClick={() => handleClick('sub')}>
+          -
+        </div>
       </div>
     </div>
   );
